8dd968e3ea12e7a9ab8d5ddafc20fa71ac337a76
Anton <antondonohue2@protonmail.com>
2021-03-03 22:26:50 +1000

    added some setter getter methods added method for inserting a piece in a specific row changed method names to align with PEP8

--- connect4.py
+++ connect4.py
@@ -5,15 +5,43 @@ class Board:
         self.length = length
         self.width = width
         self.board = [['-' for i in range(self.width)] for j in range(self.length)]
-    def printBoard(self):
-        for ind, spots in enumerate(self.board):
+    def insert_piece(self, team, column):
+        insertionRow = -1
+        insertionColumn = -1
+        for row in range(self.length):
+            if self.get_square(row, column) == '-':
+                insertionRow = row
+                insertionColumn = column
+        if insertionRow == -1 and insertionColumn == -1:
+           return 0
+        self.set_square(insertionRow, insertionColumn, team)
+        return 1
+                
+
+
+
+    def get_board(self):
+        return self.board
+
+    def get_square(self, length, width):
+        return self.board[length][width]
+
+    def set_square(self, length, width, value):
+        self.board[length][width] = value
+
+    def print_board(self):
+        for ind, spots in enumerate(self.get_board()):
             for spot in spots:
                 print(spot, end = " ")
             print("\n")
-        for ind, column in enumerate(range(self.width)):
-            print(chr(ind + ord('A')), end = " ")
+        for column in range(self.width):
+            print(chr(column + ord('A')), end = " ")
             
                     
 b = Board(6,10)
-b.printBoard()
+b.insert_piece('X', 4)
+b.insert_piece('X', 4)
+b.insert_piece('X', 4)
+b.insert_piece('X', 3)
+b.print_board()
 
